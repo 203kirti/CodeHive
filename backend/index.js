2659f950c41696ff9f80550bf5826f7839f21010
Kirti <kirti.air203@gmail.com>
2025-02-20 00:53:06 +0530

    creating room

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import { Socket } from "dgram";
 import express from "express"
 import http from "http"
 import { Server } from "socket.io"
@@ -12,11 +13,41 @@ const io = new Server(server, {
   }
 });
 
+const rooms = new Map();
+
+
 io.on("connection", (socket) => {
   console.log("User Connected",socket.id);
   socket.on("disconnect", () => {  // Extra part later we will remove it
     console.log("user disconnected");
   });
+
+  let currentRoom = null;
+  let currentUser = null;
+
+  socket.on("join" , ({roomId , userName}) => {
+    if(currentRoom){
+      socket.leave(currentRoom)
+      rooms.get(currentRoom).delete(currentUser)
+      io.to(currentRoom).emit("userJoined" , Array.from(rooms.get(currentRoom)));
+    }
+
+    currentRoom = roomId;
+    currentUser = userName;
+
+    socket.join(roomId);
+
+    if( !rooms.has(roomId)){
+      rooms.set(roomId , new Set());
+    }
+
+    roomId.get(roomId).add(userName)
+
+    io.to(roomId).emit("userJoined" , Array.from(rooms.get(currentRoom)));
+
+    
+
+  })
 })
 
 const port = process.env.PORT || 5050;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import io from "socket.io-client";
 
 const soket = io("http://localhost:5050");
 
 const App = () => {
-  return <div>Code Hive</div>;
+
+  const [joined , setJoined] = useState(false);
+  const [roomId , setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+
+
+  if(!joined){
+    return 
+    <div className="join-container">
+      <div className="join-form">
+        <h1> Join Code Room</h1>
+        <input
+          type="text"
+          placeholder="Room Id"
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+    </div>;
+  }
+  
+  return <div>user joined</div>
 };
 
 export default App;
